5e23f99584b62e65901063ae9eec4bc0793375a4
thisShandy <shandyrix@gmail.com>
2021-07-27 18:57:24 +0300

    starting develop HomeScreen

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,15 +1,12 @@
 import React, { useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
 
 import HomeScreens from '../navigation/HomeScreens'
 import AuthScreens from '../navigation/AuthScreens'
 
-const Stack = createStackNavigator()
-
 export default function MainScreen() {
 
-    const [ isAuth, setIsAuth ] = useState(false)
+    const [ isAuth, setIsAuth ] = useState(true)
 
     return (
         <NavigationContainer>
--- src/screens/menuScreens/HomeScreen.js
+++ src/screens/menuScreens/HomeScreen.js
@@ -1,10 +1,50 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+
+import { colors } from '../../theme/color'
 
 export default function HomeScreen() {
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={styles.container}>
+            <View style={styles.profileContainer}>
+                <View style={styles.userContainer}>
+                    <Text style={{}}>Добро пожаловать</Text>
+                    <Text style={{fontSize: 16, fontWeight: '700'}}>Ник пользователя</Text>
+                </View>
+                <View style={styles.messengerContainer}>
+                    <TouchableOpacity style={styles.messengerButton}>
+                        <Feather name="message-circle" size={26} color="black" />
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    profileContainer: {
+        width: '100%',
+        height: '15%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: '10%',
+        backgroundColor: 'red',
+    },
+    userContainer: {
+        top: 15,
+    },
+    messengerButton: {
+        width: 40,
+        height: 40,
+        top: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        backgroundColor: colors.SECOND_COLOR,
+    }
+})  
